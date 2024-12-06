084f7267ac004d3d3bda4f8fb713b98c25959edc
Celeste Acosta <cele7acosta@gmail.com>
2024-12-05 22:20:29 -0300

    Agregar archivos de proyecto.

---
+++ Models/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiWebAPI.Models
+{
+    public class Empleado
+    {
+        [Key]
+        public int IdEmpleado { get; set; }
+        public string? NombreCompleto { get; set; }
+        public string? Correo { get; set; }
+        public decimal Sueldo { get; set; }
+        public string? FechaContrato { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using MiWebAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<EmpleadoData>();
+
+//para que no entren en conflictos las urls del front y el back
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("NuevaPolitica", app =>
+    {
+        app.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("NuevaPolitica"); //para que use la nueva politica
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
